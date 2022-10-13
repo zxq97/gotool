2cd0dc5553da475029fe53e34f2ed15ceab933c0
zongxingquan <zongxingquan@126.com>
2022-10-13 21:04:07 +0800

    fix redis ex string

--- redisx/string.go
+++ redisx/string.go
@@ -2,13 +2,16 @@ package redisx
 
 import (
 	"context"
+	"github.com/go-redis/redis/v8"
 	"time"
 )
 
 func (rx *RedisX) IncrByXEX(ctx context.Context, key string, incr int64, ttl time.Duration) error {
 	ok, err := rx.Expire(ctx, key, ttl).Result()
-	if err != nil || !ok {
+	if err != nil {
 		return err
+	} else if !ok {
+		return redis.Nil
 	}
 	return rx.IncrBy(ctx, key, incr).Err()
 }
