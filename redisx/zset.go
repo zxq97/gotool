3e43b78dfa76ce49e2c41fab113616f6570817ae
zongxingquan <zongxingquan@126.com>
2022-11-12 23:48:58 +0800

    redis rev by mem withscores

--- redisx/zset.go
+++ redisx/zset.go
@@ -16,9 +16,21 @@ var (
         local rank = redis.call("ZRevRank", KEYS[1], ARGV[1])
         if not rank then
             rank = 0
+		else
+			rank = rank + 1
         end
-        return redis.call("ZRevRange", KEYS[1], rank, rank+ARGV[2])
+        return redis.call("ZRevRange", KEYS[1], rank, rank + ARGV[2])
     `)
+
+	zRevRangeByMemberWithScoresScript = redis.NewScript(`
+		local rank = redis.call("ZRevRank", KEYS[1], ARGV[1])
+		if not rank then
+			rank = 0
+		else
+			rank = rank + 1
+		end
+		return redis.call("ZRevRange", KEYS[1], rank, rank + ARGV[2], 'withscores')
+	`)
 )
 
 func (rx *RedisX) ZAddXEX(ctx context.Context, key string, zs []*redis.Z, ttl time.Duration) error {
@@ -59,4 +71,21 @@ func (rx *RedisX) ZRevRangeByMember(ctx context.Context, key, member string, off
 		ids = append(ids, cast.ParseInt(id, 0))
 	}
 	return ids, nil
+}
+
+func (rx *RedisX) ZRevRangeByMemberWithScores(ctx context.Context, key, member string, offset int64) ([]*redis.Z, error) {
+	res, err := zRevRangeByMemberWithScoresScript.Run(ctx, rx, []string{key}, member, offset).Result()
+	if err != nil {
+		return nil, err
+	}
+	val, ok := res.([]interface{})
+	if !ok || len(val) == 0 || len(val)&1 != 0 {
+		return nil, redis.Nil
+	}
+	zs := make([]*redis.Z, 0, len(val)>>1)
+	for i := 0; i < len(val); i += 2 {
+		id := val[i+1].(string)
+		zs = append(zs, &redis.Z{Member: val[i], Score: float64(cast.ParseInt(id, 0))})
+	}
+	return zs, nil
 }
