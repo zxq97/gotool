d1b9ec0a3ba4fabe5fdab0c501ba9529636d3035
zongxingquan <zongxingquan@126.com>
2023-01-09 18:23:33 +0800

    kafka send msg

--- kafka/pruducer.go
+++ kafka/pruducer.go
@@ -1,9 +1,13 @@
 package kafka
 
 import (
+	"context"
 	"log"
 
 	"github.com/Shopify/sarama"
+	"github.com/golang/protobuf/proto"
+	"github.com/zxq97/gotool/constant"
+	"github.com/zxq97/gotool/generate"
 )
 
 type Producer struct {
@@ -28,7 +32,7 @@ func InitKafkaProducer(addr []string, apiLogger, excLogger *log.Logger) (*Produc
 	return &Producer{producer: producer, apiLogger: apiLogger}, nil
 }
 
-func (producer *Producer) SendMessage(topic string, key []byte, data []byte) error {
+func (producer *Producer) sendMessage(topic string, key []byte, data []byte) error {
 	partition, offset, err := producer.producer.SendMessage(&sarama.ProducerMessage{
 		Topic: topic,
 		Key:   sarama.ByteEncoder(key),
@@ -40,6 +44,27 @@ func (producer *Producer) SendMessage(topic string, key []byte, data []byte) err
 	}
 	producer.apiLogger.Println("SendMessage partition offset date", partition, offset, string(data))
 	return nil
+}
+
+func (producer *Producer) SendKafkaMsg(ctx context.Context, topic, key string, req proto.Message, eventType int32) error {
+	trace, ok := ctx.Value(constant.TraceIDKey).(string)
+	if !ok {
+		trace = generate.UUIDStr()
+	}
+	bs, err := proto.Marshal(req)
+	if err != nil {
+		return err
+	}
+	kfkmsg := &KafkaMessage{
+		TraceId:   trace,
+		EventType: eventType,
+		Message:   bs,
+	}
+	bs, err = proto.Marshal(kfkmsg)
+	if err != nil {
+		return err
+	}
+	return producer.sendMessage(topic, []byte(key), bs)
 }
 
 func (producer *Producer) Stop() error {
